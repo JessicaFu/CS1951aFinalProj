da59676dcfc1d76e7459fb69172c51b10e9319de
Ubuntu <ubuntu@ip-172-31-60-119.ec2.internal>
2015-04-10 03:47:08 +0000

    models.py started

--- news/models.py
+++ news/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Source(models.Model):
+    name = models.CharField(max_length=16)
+
+
+class Article(models.Model):
+    source = models.ForeignKey(Source)
+    date = models.DateTimeField('date published')
+    url = models.URLField()
+    text = models.TextField()
+
+class Keyword(models.Model):
+    article = models.ForeignKey(Article)
+    word = models.CharField(max_length=24)
+
+class RedditPost(models.Model):
+    article = models.ForeignKey(Article)
+    votes = models.Integer()
+
+class RedditComment(models.Model):
+    reddit_post = models.ForeignKey(RedditPost)
+    text = models.CharField(max_length=200)
+    parent_comment = models.ForeignKey(RedditComment)
