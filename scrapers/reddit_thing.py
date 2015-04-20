64bb7ecebdfc3b8a94f5825e3622061ce6e11f71
Benjamin Bauer <benjamin_d_bauer@brown.edu>
2015-04-20 15:33:48 -0400

    Updated time calculation in reddit script

--- scrapers/reddit_thing.py
+++ scrapers/reddit_thing.py
@@ -6,6 +6,7 @@ from django.db import models
 from news.models import *
 from datetime import datetime
 import praw
+import time
 
 
 r = praw.Reddit(user_agent="data_science_project")
@@ -15,8 +16,7 @@ for submission in subreddit.get_hot(limit=100): #May want to change limit=1 to s
 	post_title = submission.title
 	post_url = submission.url #Sometimes this just links to an image, or reddit post. It's not always an article.
 	#We'll have to turn this into an actual date. Looking at some other scripts, it looks like someone has already figured out how to do that. If not, I can work on it later, just let me know.
-	post_created_at = submission.created 
-	post_created_at = submission.created_utc	
+	post_created_at = time.localtime(submission.created_utc)
 
 
 	forest_of_comments = submission.comments
@@ -28,8 +28,7 @@ for submission in subreddit.get_hot(limit=100): #May want to change limit=1 to s
 		comment_score = comment.score
 		
 		#Similar thing here. Time needs to be converted to actual date.
-		comment_date = datetime.strptime(comment.created)
-		comment.created_utc
+		comment_date = time.localtime(comment.created_utc)
                 comments.append({
                     'text' : comment_text,
                     'creation_time' : comment_date
