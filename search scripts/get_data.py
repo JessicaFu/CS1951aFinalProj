0fc596a25f66ece95976b684463e2798fcc7c8f0
Ubuntu <ubuntu@ip-172-31-28-135.ec2.internal>
2015-05-06 21:17:05 +0000

    Changes to country/city counts

--- search scripts/get_data.py
+++ search scripts/get_data.py
@@ -7,69 +7,20 @@ from news.models import *
 
 import datetime
 import operator
+import csv
 
 ##########################################################################
-################# Build a collection of cities/countries #################
+################# Retrieve Article metadata ##############################
 ##########################################################################
 
-# Create a dictionary of countries along w/ their (lat,lon) coordinates
-country_coords = {}
-country_file = open('countries.tsv')
-country_file.next()
-country_file.next()
-for line in country_file:
-	elts = line.split('\t')
-	country_coords[elts[3].lower().strip('\n')] = (float(elts[1]), float(elts[2]))
+with open('search_data.csv', 'w') as csvfile:
+	csv_writer = csv.writer(csvfile)
+	csv_writer.writerow([])
 
-# Set of countries set to be keys of dictionary
-countries = country_coords.keys()
-
-# Dictionary of cities in the world along w/ their (lat,lon) coordinates
-city_coords = {}
-city_file = open('cities_big.csv')
-city_file.next()
-for line in city_file:
-    elts = line.split(',')
-    if elts[2].lower().strip('\n') == 'man':
-        print line
-    city_coords[elts[2].lower().strip('\n')] = (float(elts[0]), float(elts[1]))
-
-# City set is the keys of this dictionary
-cities = city_coords.keys()
-cities.remove('')
-cities.remove('man')
-#for city in cities:
-#	print city
-print str(len(cities)) + ' cities'
-
-##########################
-# Add some more cities from another file
-# This has a mapping from city to country if desired
-#city_coords = {}
-#cities = []
-#new_cities = 0
-#cc_file = open('more_cities.csv')
-#cc_file.next()
-#for line in cc_file:
-#	elts = line.split(',')
-#	country = elts[1].lower().strip('"')
-#	city = elts[2].lower().strip('"')
-#	lat = float(elts[3].strip('"'))
-#	lon = float(elts[4].strip('"'))
-#	if not city in cities:
-#		city_coords[city] = (lat, lon)
-#		new_cities += 1
-
-#print 'man' in cities 
-#cities = city_coords.keys()
-#print new_cities
-#print str(len(cities)) + ' cities'
-
-#for city in cities:
-#	if 'x' in city:
-#		print city
-
-
+articles = Article.objects.all()
+count = 0
+for art in articles: 	
+	print art
 ###################################################################
 ################# Find Trending cities/countries ##################
 ###################################################################
